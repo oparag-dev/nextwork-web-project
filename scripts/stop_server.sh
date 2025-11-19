a17ba6447344a0c91676357f463d1a68a506e0c5
EC2 Default User <ec2-user@ip-172-31-36-221.eu-west-3.compute.internal>
2025-11-19 15:34:43 +0000

    reverting to previous settings on stop_server

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-#!/bin/bash
-# Add this at the top of your script to make it exit immediately if any command fails
-set -e
-
-# Your intentional error
-sudo systemctll stop httpd.service  # Misspelled command will now cause script to exit with non-zero status
-
-# To be extra sure, we can also add an explicit exit code
-exit 1
+sudo systemctl start tomcat.service
+sudo systemctl enable tomcat.service
+sudo systemctl start httpd.service
+sudo systemctl enable httpd.service
